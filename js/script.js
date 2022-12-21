98e56bd61f2bff24e13f943a8a0d7853902154ea
Stepan Andreev <stepan10andreev@gmail.com>
2022-12-21 20:36:53 +0300

    add js-breakpoint-gallery-swiper

--- js/script.js
+++ js/script.js
@@ -11,11 +11,32 @@ const swiper = new Swiper('.swiper', {
   slidesPerGroup: 3,
   spaceBetween: 50,
   loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 38
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 34
+    },
+    1660: {
+      slidesPerView: 3,
+      spaceBetween: 50
+    }
+  },
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
   pagination: {
     el: '.swiper-pagination',
     type: 'fraction',
