45964621ab190db8b8195efcf3cb3e5ece2b0a88
Stepan Andreev <stepan10andreev@gmail.com>
2022-12-25 10:29:46 +0300

    add events

--- js/script.js
+++ js/script.js
@@ -66,5 +66,54 @@ tabsBtn.forEach(function(element){
     tabsItem.forEach(function(element){ element.classList.remove('tabs-item--active')});
     document.querySelector(`[data-target="${path}"]`).classList.add('tabs-item--active');
   })
-});
+})
+
+
+var swiper2 = new Swiper ('.events-swiper', {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 50,
+  loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 38
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 27,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      }
+    },
+    1660: {
+      slidesPerView: 3,
+      spaceBetween: 50
+    }
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next-events',
+  },
+
+  pagination: {
+    el: '.swiper-pagination-events',
+    type: 'bullets',
+		clickable: true
+  }
+})
+
+const swiperNext = document.getElementById('.swiper-button-next-events')
+
+swiperNext.addEventListener('click', () => {
+  swiper2.slideNext();
+})
 
