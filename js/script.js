c3a21901d596a198c0c16df54b36196b4cbf95a9
Stepan Andreev <stepan10andreev@gmail.com>
2022-12-27 18:27:23 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -258,3 +258,17 @@ swiper3Next.addEventListener('click', () => {
     })
 
 
+    var modal = document.querySelector("#modal");
+    var modalOverlay = document.querySelector("#modal-overlay");
+    var closeButton = document.querySelector(".modal__btn-cancel");
+    var openButton = document.querySelector(".btn-open-malevich");
+
+    closeButton.addEventListener("click", function() {
+      modal.classList.toggle("closed");
+      modalOverlay.classList.toggle("closed");
+    });
+
+    openButton.addEventListener("click", function() {
+      modal.classList.toggle("closed");
+      modalOverlay.classList.toggle("closed");
+    });
