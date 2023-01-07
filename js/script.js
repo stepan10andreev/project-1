f78ed78e4291a6a4145fe049d797e68dc22badf1
Stepan Andreev <stepan10andreev@gmail.com>
2023-01-07 09:11:23 +0300

    add validate

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const choices = new Choices(element, {
   allowHTML: true,
 	shouldSort: false,
   position: 'bottom'
-})
+});
 
 
 const swiper = new Swiper('.swiper', {
@@ -57,7 +57,7 @@ const swiper = new Swiper('.swiper', {
     type: 'fraction',
 		clickable: true,
   }
-})
+});
 
 
 
@@ -66,7 +66,7 @@ new Accordion('.accordion-list', {
 	triggerClass: 'accordion__control',
 	panelClass: 'accordion__content',
 	activeClass: 'accordion--active'
-})
+});
 
 
 
@@ -83,7 +83,7 @@ tabsBtn.forEach(function(element){
     tabsItem.forEach(function(element){ element.classList.remove('tabs-item--active')});
     document.querySelector(`[data-target="${path}"]`).classList.add('tabs-item--active');
   })
-})
+});
 
 
 var swiper2 = new Swiper ('.events-swiper', {
@@ -125,15 +125,13 @@ var swiper2 = new Swiper ('.events-swiper', {
     type: 'bullets',
 		clickable: true
   }
-})
+});
 
 const swiper2Next = document.querySelector('.swiper-button-next-events')
 
 swiper2Next.addEventListener('click', () => {
   swiper2.slideNext();
-})
-
-
+});
 
 
 var swiper3 = new Swiper ('.projects-swiper', {
@@ -176,117 +174,113 @@ var swiper3 = new Swiper ('.projects-swiper', {
     prevEl: '.swiper-button-prev-projects',
   },
 
-})
+});
 
 const swiper3Prev = document.querySelector('.swiper-button-prev-projects')
 
 swiper3Prev.addEventListener('click', () => {
   swiper3.slidePrev();
-})
+});
 
 const swiper3Next = document.querySelector('.swiper-button-next-projects')
 
 swiper3Next.addEventListener('click', () => {
   swiper3.slideNext();
-})
+});
 
 
 
-  var selector = document.querySelector("input[type='tel']");
-  var im = new Inputmask("+7(999) 999-99-99");
-  im.mask(selector);
+var selector = document.querySelector("input[type='tel']");
+var im = new Inputmask("+7(999) 999-99-99");
+im.mask(selector);
 
-  new JustValidate('.contacts-form', {
-      rules: {
-          name: {
-              required: true,
-              minLength: 2,
-              maxLength: 10
-          },
-          tel: {
-              required: true,
-              function: (name, value) => {
-                  const phone = selector.inputmask.unmaskedvalue()
-                  console.log(phone)
-                  return Number(phone) && phone.length === 10
-              }
-          }
-      },
-      messages: {
-          name: {
-              required:"Вы не ввели имя",
-              minLength: "Поле должно содержать минимум 2 символа",
-              maxLength: "Поле должно содержать максимум 10 символов"
-          },
-          tel: {
-              required: "Вы не ввели телефон",
-              function: "Вы не ввели телефон"
-          }
-      },
+new JustValidate('.contacts-form', {
+    rules: {
+        name: {
+            required: true,
+            minLength: 2,
+            maxLength: 10
+        },
+        tel: {
+            required: true,
+            function: (name, value) => {
+                const phone = selector.inputmask.unmaskedvalue()
+                console.log(phone)
+                return Number(phone) && phone.length === 10
+            }
+        }
+    },
+    messages: {
+        name: {
+            required:"Вы не ввели имя",
+            minLength: "Поле должно содержать минимум 2 символа",
+            maxLength: "Поле должно содержать максимум 10 символов"
+        },
+        tel: {
+            required: "Вы не ввели телефон",
+            function: "Вы не ввели телефон"
+        }
+    },
 
-        submitHandler: function(thisForm) {
-          let formData = new FormData(thisForm);
+      submitHandler: function(thisForm) {
+        let formData = new FormData(thisForm);
 
-          let xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
 
-          xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-              if (xhr.status === 200) {
-                console.log('Отправлено');
-              }
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              console.log('Отправлено');
             }
           }
-
-          xhr.open('POST', 'mail.php', true);
-          xhr.send(formData);
-
-          thisForm.reset();
         }
 
-  })
+        xhr.open('POST', 'mail.php', true);
+        xhr.send(formData);
+
+        thisForm.reset();
+      }
+});
 
 
+let menuLink = document.querySelectorAll('.menu__link');
+let dropdownList = document.querySelectorAll('.dropdown__list');
+let linkSvg = document.querySelectorAll('.menu-link__svg');
 
 
+menuLink.forEach(function(link){
+  link.addEventListener('click', function(drop){
+    const path = drop.currentTarget.dataset.path;
 
-    let menuLink = document.querySelectorAll('.menu__link');
-    let dropdownList = document.querySelectorAll('.dropdown__list');
-    let linkSvg = document.querySelectorAll('.menu-link__svg');
+    menuLink.forEach(function(svg){ svg.classList.remove('btn--active')});
+    drop.currentTarget.classList.add('btn--active');
 
-
-    menuLink.forEach(function(link){
-      link.addEventListener('click', function(drop){
-        const path = drop.currentTarget.dataset.path;
-
-        menuLink.forEach(function(svg){ svg.classList.remove('btn--active')});
-        drop.currentTarget.classList.add('btn--active');
-
-        drop.currentTarget.addEventListener('click', function(dropTwo){
-          document.querySelector(`[data-target="${path}"]`).classList.toggle('dropdown__list--active');
-          dropTwo.currentTarget.classList.toggle('btn--active');
-        })
-
-        dropdownList.forEach(function(link){ link.classList.remove('dropdown__list--active')});
-        document.querySelector(`[data-target="${path}"]`).classList.add('dropdown__list--active');
-
-      })
+    drop.currentTarget.addEventListener('click', function(dropTwo){
+      document.querySelector(`[data-target="${path}"]`).classList.toggle('dropdown__list--active');
+      dropTwo.currentTarget.classList.toggle('btn--active');
     })
 
+    dropdownList.forEach(function(link){ link.classList.remove('dropdown__list--active')});
+    document.querySelector(`[data-target="${path}"]`).classList.add('dropdown__list--active');
 
-    var modal = document.querySelector("#modal");
-    var modalOverlay = document.querySelector("#modal-overlay");
-    var closeButton = document.querySelector(".modal__btn-cancel");
-    var openButton = document.querySelector(".btn-open-malevich");
+  })
+});
 
-    closeButton.addEventListener("click", function() {
-      modal.classList.toggle("closed");
-      modalOverlay.classList.toggle("closed");
-    });
 
-    openButton.addEventListener("click", function() {
-      modal.classList.toggle("closed");
-      modalOverlay.classList.toggle("closed");
-    });
+var modal = document.querySelector("#modal");
+var modalOverlay = document.querySelector("#modal-overlay");
+var closeButton = document.querySelector(".modal__btn-cancel");
+var openButton = document.querySelector(".btn-open-malevich");
+
+closeButton.addEventListener("click", function() {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
+
+openButton.addEventListener("click", function() {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
 
 
 let burger = document.querySelector('.burger');
@@ -302,7 +296,7 @@ burger.addEventListener('click',
     menu.classList.toggle('header__nav--active');
 
     document.body.classList.toggle('stop-scroll');
-})
+});
 
 menuLinks.forEach(function(el){
   el.addEventListener('click', function() {
@@ -312,7 +306,7 @@ menuLinks.forEach(function(el){
 
     document.body.classList.remove('stop-scroll');
   })
-})
+});
 
 
 let searchBtn = document.querySelector('.header__btn-search');
@@ -320,16 +314,15 @@ let searchForm = document.querySelector('.search-form-modal');
 let canselBtn = document.querySelector('.search-form-modal__cansel')
 
 searchBtn.addEventListener('click',
-
-function() {
-  searchForm.classList.add('search-form--active');
-})
+  function() {
+    searchForm.classList.add('search-form--active');
+});
 
 
 canselBtn.addEventListener('click',
 function(){
   searchForm.classList.remove('search-form--active')
-})
+});
 
 
 var catalogArticle = document.querySelectorAll(".catalog__article");
